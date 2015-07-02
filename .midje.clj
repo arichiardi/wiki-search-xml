c62e3d522f9fd880d75cf0f584a8fe644bc6a73a
Andrea Richiardi <a.richiardi.work@gmail.com>
2015-07-02 15:12:54 +0200

    Add conf to .midje.clj so that we don't waste time automatically testing :slow and :bench tests.

--- .midje.clj
+++ .midje.clj
@@ -1,1 +1,5 @@
 (change-defaults :print-level :print-namespaces)
+
+(when-not (running-in-repl?)
+  (change-defaults :fact-filter #(and (not (:slow %))
+                                      (not (:bench %)))))
